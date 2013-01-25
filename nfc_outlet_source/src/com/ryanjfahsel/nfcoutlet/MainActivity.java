482d548094d8d70c371f131974420c7337cdf3eb
rfahsel3 <ryan.fahsel3@gmail.com>
2013-01-25 14:10:27 -0500

    added background processing. not done

---
+++ nfc_outlet_source/src/com/ryanjfahsel/nfcoutlet/MainActivity.java
@@ -0,0 +1,57 @@
+package com.ryanjfahsel.nfcoutlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+//I added these pkgs
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import com.example.myfirstapp.R;
+
+public class MainActivity extends Activity {
+
+	//Create new Network Activity and pass it the current activity
+	NetworkActivity network=new NetworkActivity(this);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final Button button = (Button) findViewById(R.id.button_send);
+	    button.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            // Perform action on click
+	        	network.execute();
+	        }
+	    });
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	
+
+}
---
+++ nfc_outlet_source/src/com/ryanjfahsel/nfcoutlet/NetworkActivity.java
@@ -0,0 +1,79 @@
+package com.ryanjfahsel.nfcoutlet;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+public class NetworkActivity extends AsyncTask<Void,Void,Void> {
+
+	private String st;
+	private TextView tv;
+	private MainActivity mActivity;
+	public NetworkActivity(MainActivity mActivity)
+	{
+		this.mActivity=mActivity;
+	}
+	protected Void doInBackground(Void... params) {
+		postData("www.ryanjfahsel.com/index.php", "902563529","rfahsel3","Katie sucks");
+		return null;
+	}
+	
+	protected void onPostExecute(Void result)  {
+		mActivity.findViewById();
+	}
+	
+	
+	public void postData(String postURL, String nfc_id, String username, String passwd){
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(postURL);
+		
+		try{
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+			nameValuePairs.add(new BasicNameValuePair("nfcid", nfc_id));
+			nameValuePairs.add(new BasicNameValuePair("username", username));
+			nameValuePairs.add(new BasicNameValuePair("passwd", passwd));
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			HttpResponse response = httpclient.execute(httppost);
+			String line = "";
+		    StringBuilder total = new StringBuilder();
+		    
+		    // Wrap a BufferedReader around the InputStream
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		    // Read response until the end
+		    while ((line = rd.readLine()) != null) { 
+		        total.append(line); 
+		    }
+			st=total.toString();
+			Log.w("Response:", "GOT HERE");
+		    Log.w("Response: ", total.toString());
+			Log.w("RESPONSE", "GOT HERE");
+		}
+		catch(ClientProtocolException e){
+			//TODO
+		}
+		catch(IOException e){
+			//TODO
+		}
+	}
+
+}
+	
+	
+	
+
