dfe12fb5b144b0d0c3234193e3d1131992cd7c7c
rfahsel3 <ryan.fahsel3@gmail.com>
2013-02-06 02:17:15 -0500

    Added welcome message

--- webserver/index.php
+++ webserver/index.php
@@ -1,3 +1,4 @@
 <?php
-echo $_REQUEST['nfcid'].",".$_REQUEST['username'].",".$_REQUEST['password'];
+//echo $_REQUEST['nfcid'].",".$_REQUEST['username'].",".$_REQUEST['password'];
+echo "This is the homepage for the NFC server. It is up and working";
 ?>
