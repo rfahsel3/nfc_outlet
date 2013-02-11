9adf9d3cde405c0f59c50275af4b7e2ae3794035
Ramya <ram.ramya@gmail.com>
2013-02-11 01:45:06 -0500

    Added a few lines to the index file to get a feel for php
    Please let me know if I'm doing this right!

--- webserver/index.php
+++ webserver/index.php
@@ -1,4 +1,11 @@
 <?php
 //echo $_REQUEST['nfcid'].",".$_REQUEST['username'].",".$_REQUEST['password'];
 echo "This is the homepage for the NFC server. It is up and working";
+echo "<p>Here, you will be able to add users and manage permissions.</p>";
+
+$addUser = "<input type='button' id='php_button' value='Add User'>";
+echo $addUser;
+
+$manage = "<input type='button' id='php_button' value='Manage Permissions'>";
+echo $manage;
 ?>
