8e406d74492888e0de9ee76727b13e36aab40584
Ramya <ram.ramya@gmail.com>
2013-02-19 12:51:26 -0500

    Can delete users and navigate through different pages of site

--- webserver/userProcess.php
+++ webserver/userProcess.php
@@ -1,5 +1,7 @@
 <?php
 require("res\setup.php");
 mysql_query("INSERT INTO muc.Users (username, password, admin) VALUES ('$_REQUEST[username]', '$_REQUEST[password]', 0)");
-echo "Congratulations! You have now created an account!";
+echo "</br>Congratulations! You have now created an account!</br>";
+$goBack = "<input type='button' id='back_button' value='Back to Users'>";
+echo "<a href='users.php'>".$goBack."</a></br>";
 ?>
--- webserver/users.php
+++ webserver/users.php
@@ -1,15 +1,23 @@
 <?php
 require("res/setup.php");
-$query = mysql_query("SELECT username from muc.Users");
-echo "<table border=1>";
-echo "<tr><th>Username</th></tr>";
+$query = mysql_query("SELECT * from muc.Users");
+echo "<table border=1 cellpadding=5 cellspacing=2>";
+echo "<tr><th>Username</th></tr>"; //<th></th></tr>";
+$num=0;
 while($rows = mysql_fetch_array($query)){
-	echo "<tr><td>".$rows['username']."</td></tr>";
+	$num++;
+	$value=$rows['uid'];
+	echo "<form action='delete.php' method='post'>";
+	echo "<input type=hidden id=$value name='id' value='$value'>";
+	$delete = "<input type='submit' value='Delete'></form>";
+	echo "<tr><td>".$rows['username']."</td><td><a href='users.php'>".$delete."</a></td></tr>";
 }
+
 echo "</table>";
 echo "<form action='userProcess.php' method='post'>";
-echo "Username: <input type='text' name='username'></br>";
+echo "</br>Username: <input type='text' name='username'></br>";
 echo "Password: <input type='password' name='password'></br>";
 echo "<input type='submit'>";
-
+$goBack = "<input type='button' id='back_button' value='Back to Home'>";
+echo "<a href='index.php'>".$goBack."</a></br>";
 ?>
