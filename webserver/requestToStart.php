e6431460ae8f18b9f6feda11eeba264eec2d6b0f
Colin Gray <colin.gray@gatech.edu>
2013-04-15 17:27:34 -0400

    Updated requestToStart to update count in Permissions table

--- webserver/requestToStart.php
+++ webserver/requestToStart.php
@@ -6,12 +6,14 @@ require('res/setup.php');
 $nfcid=$_REQUEST[nfcid];
 $username=$_REQUEST[username];
 $password=$_REQUEST[password];
-$query=mysql_query("SELECT Tools.toolName, Tools.description, Tools.tid FROM Permissions join Tools on Permissions.tid=Tools.tid join Users on Permissions.uid=Users.uid WHERE Users.username='$username' AND Users.password='$password' AND Tools.nfcid='$nfcid'");
+$query=mysql_query("SELECT Tools.toolName, Tools.description, Tools.tid, Permissions.count FROM Permissions join Tools on Permissions.tid=Tools.tid join Users on Permissions.uid=Users.uid WHERE Users.username='$username' AND Users.password='$password' AND Tools.nfcid='$nfcid'");
 if($query)	{
 	$result=mysql_fetch_array($query);
 	if(mysql_num_rows($query)==1)	{
 		echo "1".",".$result[2].",".$result[0].",".$result[1];
 		mysql_query("UPDATE Tools SET enable=1 WHERE nfcid='$nfcid'");
+		$count = $result[3]+1;
+		mysql_query("UPDATE Permissions SET count='$count' WHERE tid='$result[2]'");
 	}
 	else	{
 		echo "You are not authorized to use this tool. If you believe this is in error please contact an administrator.";
