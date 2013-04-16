7b242041542a97b924f69baf5f34ef658a701ca6
root <root@raspberrypi.(none)>
2013-04-15 20:10:58 -0400

    Adding XBEE stuff

---
+++ java/xbee/.sh
@@ -0,0 +1,1 @@
+java -classpath bin/:lib/RXTXcomm.jar:lib/log4j.jar com.rapplogic.xbee.examples.RemoteAtExample
---
+++ java/xbee/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -classpath bin/:lib/RXTXcomm.jar:lib/log4j.jar com.rapplogic.xbee.examples.RemoteAtExample
