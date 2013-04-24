2b3bf1c2f77281faf4ff3b085dc79403ad2eb12d
root <root@ryanjfahsel.com>
2013-04-23 23:36:37 -0400

    Fixed user problem because of wrong slashes

--- webserver/userProcess.php
+++ webserver/userProcess.php
@@ -1,5 +1,5 @@
 <?php
-require("res\setup.php");
+require("res/setup.php");
 mysql_query("INSERT INTO muc.Users (username, password, admin) VALUES ('$_REQUEST[username]', '$_REQUEST[password]', 0)");
 echo "</br>Congratulations! You have now created an account!</br>";
 $goBack = "<input type='button' id='back_button' value='Back to Users'>";
